3296f79dfe3390084f5f0dc797b4e7df7d62adad
bradenr402 <bradenr402@gmail.com>
2024-03-25 16:26:07 -0400

    fix: eslint camelcase

--- src/fetch-weather-data.js
+++ src/fetch-weather-data.js
@@ -1,9 +1,9 @@
-const weather_api_key = '3b5e7413d81949a3b17232658242103';
+const API_KEY = '3b5e7413d81949a3b17232658242103';
 
 export default async function fetchWeatherData(search) {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${weather_api_key}&q=${search}`,
-    { mode: 'cors' }
+    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${search}`,
+    { mode: 'cors' },
   );
   const data = await response.json();
   console.log(data);
