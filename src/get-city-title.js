a2219b9ba978dd7c7315a4d44dd9b17a2c073ff6
bradenr402 <bradenr402@gmail.com>
2024-03-25 17:18:29 -0400

    add US state and Canadian province abbreviations

--- src/get-city-title.js
+++ src/get-city-title.js
@@ -1,5 +1,86 @@
 import saveLastLocation from './save-last-location';
 
+const regionAbbreviations = {
+  'United States of America': {
+    Alabama: 'AL',
+    Alaska: 'AK',
+    Arizona: 'AZ',
+    Arkansas: 'AR',
+    California: 'CA',
+    Colorado: 'CO',
+    Connecticut: 'CT',
+    Delaware: 'DE',
+    Florida: 'FL',
+    Georgia: 'GA',
+    Hawaii: 'HI',
+    Idaho: 'ID',
+    Illinois: 'IL',
+    Indiana: 'IN',
+    Iowa: 'IA',
+    Kansas: 'KS',
+    Kentucky: 'KY',
+    Louisiana: 'LA',
+    Maine: 'ME',
+    Maryland: 'MD',
+    Massachusetts: 'MA',
+    Michigan: 'MI',
+    Minnesota: 'MN',
+    Mississippi: 'MS',
+    Missouri: 'MO',
+    Montana: 'MT',
+    Nebraska: 'NE',
+    Nevada: 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    Ohio: 'OH',
+    Oklahoma: 'OK',
+    Oregon: 'OR',
+    Pennsylvania: 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    Tennessee: 'TN',
+    Texas: 'TX',
+    Utah: 'UT',
+    Vermont: 'VT',
+    Virginia: 'VA',
+    Washington: 'WA',
+    'West Virginia': 'WV',
+    Wisconsin: 'WI',
+    Wyoming: 'WY',
+    'District of Columbia': 'DC',
+  },
+  Canada: {
+    Alberta: 'AB',
+    'British Columbia': 'BC',
+    Manitoba: 'MB',
+    'New Brunswick': 'NB',
+    'Newfoundland and Labrador': 'NL',
+    'Nova Scotia': 'NS',
+    Ontario: 'ON',
+    'Prince Edward Island': 'PE',
+    Quebec: 'QC',
+    Saskatchewan: 'SK',
+    'Northwest Territories': 'NT',
+    Nunavut: 'NU',
+    Yukon: 'YT',
+  },
+};
+
+function formattedRegionAndCountry(region, country) {
+  if (regionAbbreviations[country] && regionAbbreviations[country][region]) {
+    const abbreviation = regionAbbreviations[country][region];
+    console.log(abbreviation);
+
+    return `${abbreviation}, ${country}`;
+  }
+  return country;
+}
+
 export default function getCityTitle(location) {
   const city = location.name;
   const { region } = location;
@@ -7,8 +88,5 @@ export default function getCityTitle(location) {
 
   saveLastLocation(city, region, country);
 
-  if (country === 'United States of America' || country === 'Canada') {
-    return `${city}, ${region}, ${country}`;
-  }
-  return `${city}, ${country}`;
+  return `${city}, ${formattedRegionAndCountry(region, country)}`;
 }
