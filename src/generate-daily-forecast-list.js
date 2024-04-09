52577d0e4d38828d0f99dbbd1413b30a1be32d38
bradenr402 <bradenr402@gmail.com>
2024-04-09 13:48:21 -0400

    add weather icon to daily forecast

--- src/generate-daily-forecast-list.js
+++ src/generate-daily-forecast-list.js
@@ -16,17 +16,18 @@ function getDayName(daysInFuture) {
   return days[futureDay];
 }
 
-export default function generateDailyForecastList(index) {
+export default function generateDailyForecastList(day) {
   const forecastList = document.createElement('ul');
-  forecastList.id = `forecast-${index}`;
+  forecastList.id = `forecast-${day}`;
   forecastList.classList.add(
     'rounded-2xl',
     'text-center',
     'border',
     'border-gray-300',
     'p-6',
-    'w-[225px]',
+    'basis-[250px]',
     'flex-shrink-0',
+    'flex-grow',
     'grid',
     'grid-cols-2',
     'gap-x-2',
@@ -36,26 +37,38 @@ export default function generateDailyForecastList(index) {
   const headerDiv = document.createElement('div');
   headerDiv.classList.add('col-span-2');
 
+  const titleContainer = document.createElement('div');
+  titleContainer.classList.add('flex', 'items-center', 'justify-center');
+
+  const weatherIcon = document.createElement('img');
+  weatherIcon.src = '#';
+  weatherIcon.id = `forecast-weather-icon-${day}`;
+  weatherIcon.classList.add('mr-2', 'aspect-square', 'size-8');
+
   const dayTitle = document.createElement('h3');
   dayTitle.classList.add('font-bold', 'text-center', 'text-lg');
-  dayTitle.textContent = getDayName(index);
-  headerDiv.appendChild(dayTitle);
+  dayTitle.textContent = getDayName(day);
+
+  titleContainer.append(weatherIcon, dayTitle);
+  headerDiv.appendChild(titleContainer);
   forecastList.appendChild(headerDiv);
 
   const forecastItems = [
     { id: 'condition', title: 'Conditions' },
     { id: 'max-temp', title: 'High' },
     { id: 'min-temp', title: 'Low' },
-    { id: 'rain-chance', title: 'Chance of Rain' },
-    { id: 'total-precipitation', title: 'Precipitation' },
     { id: 'sunrise', title: 'Sunrise' },
     { id: 'sunset', title: 'Sunset' },
+    { id: 'precipitation-chance', title: 'Chance of Precipitation' },
   ];
 
   forecastItems.forEach((item) => {
     const listItem = document.createElement('li');
-    listItem.id = `${item.id}-${index}`;
+    listItem.id = `${item.id}-${day}`;
     listItem.classList.add('list-none');
+    if (item.id === 'precipitation-chance') {
+      listItem.classList.add('col-span-2');
+    }
 
     const titleSpan = document.createElement('span');
     titleSpan.className = 'sr-only';
@@ -65,16 +78,16 @@ export default function generateDailyForecastList(index) {
     containerDiv.classList.add('flex', 'flex-col');
 
     const dataSpan = document.createElement('span');
-    dataSpan.id = `${item.id}-data-${index}`;
-    dataSpan.classList.add('text-lg', 'leading-tight');
+    dataSpan.id = `${item.id}-data-${day}`;
+    if (item.id === 'condition') dataSpan.classList.add('text-lg');
+    else dataSpan.classList.add('text-2xl');
 
     const notesSpan = document.createElement('span');
-    notesSpan.id = `${item.id}-notes-${index}`;
+    notesSpan.id = `${item.id}-notes-${day}`;
     notesSpan.classList.add('text-sm', 'opacity-60');
 
     containerDiv.append(dataSpan, notesSpan);
-    listItem.append(titleSpan);
-    listItem.appendChild(containerDiv);
+    listItem.append(titleSpan, containerDiv);
     if (item.id === 'condition') headerDiv.appendChild(listItem);
     else forecastList.appendChild(listItem);
   });
