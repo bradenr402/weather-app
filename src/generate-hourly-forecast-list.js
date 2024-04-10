4643a318281ea328b7ac7118c5441c64250274a9
bradenr402 <bradenr402@gmail.com>
2024-04-10 09:51:34 -0400

    refactor `getHour` function

--- src/generate-hourly-forecast-list.js
+++ src/generate-hourly-forecast-list.js
@@ -2,35 +2,9 @@ function getHour(hour) {
   const now = new Date().getHours();
 
   if (hour === now) return 'Now';
-
-  const hours = [
-    '12:00 AM',
-    '1:00 AM',
-    '2:00 AM',
-    '3:00 AM',
-    '4:00 AM',
-    '5:00 AM',
-    '6:00 AM',
-    '7:00 AM',
-    '8:00 AM',
-    '9:00 AM',
-    '10:00 AM',
-    '11:00 AM',
-    '12:00 PM',
-    '1:00 PM',
-    '2:00 PM',
-    '3:00 PM',
-    '4:00 PM',
-    '5:00 PM',
-    '6:00 PM',
-    '7:00 PM',
-    '8:00 PM',
-    '9:00 PM',
-    '10:00 PM',
-    '11:00 PM',
-  ];
-
-  return hours[hour];
+  if (hour === 0) return '12:00 AM';
+  if (hour <= 12) return `${hour}:00 AM`;
+  return `${hour - 12} PM`;
 }
 
 export default function generateHourlyForecastList(hour) {
